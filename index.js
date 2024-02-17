320328b5bc95e5ef2e47e340e8cccb2c12e8cc67
tanzeeldilaramkhan21 <tanzeelkhandec21@gmail.com>
2024-02-17 20:43:16 +0500

    done

---
+++ index.js
@@ -0,0 +1,68 @@
+// 1. Sum of all digits of a number. For example, 123 -> 1+2+3=6
+// solution
+//let userInput = prompt("please enter a number of your choice");
+//let sum = 0;
+//for (let i = 0; i < userInput.length; i++){
+  //  sum = sum + parseInt(userInput[i]);
+//}
+//alert (sum);
+// console.log(sum);
+ 
+
+// 2. Sum of the range of 2 numbers. For example, num1=2, num2=5 -> 2+3+4+5=14
+// solution
+
+//let firstNumber = prompt("please enter  the first number of your choice");
+//let secondNumber = prompt("please enter the second number of your choice");
+//let sum = 0;
+//for (let i = firstNumber; i <= secondNumber; i++){
+//   sum = sum + parseInt (i);
+//}
+// alert(sum);
+// console.log(sum);
+
+
+// 3. Calculate the average of numbers given in an array.
+
+//let array= [23,62,74,47,74,89,92,56,16];
+//let sum = 0;
+//for (let i = 0; i < array.length; i++){
+//    sum = sum + array[i];
+//}
+//let average =  Math.round (sum / array.length);
+//alert(average);
+//console.log(average);
+
+// 4. Compare strings by count of characters. For example, car and bat are equal. car and care are not.
+
+//let string1 = prompt("please enter the first string");
+////let string2 = prompt("please enter the second string");
+// if (string1.length === string2.length) {
+ // alert("Both the strings are equal");
+
+ // console.log("Both the strings are equal");
+ // }
+ // else { 
+ // alert ("strings are not equal");
+// console.log("strings are not equal");
+//}
+
+
+// 5. Swap the string's first and last character. For example, name -> eamn
+
+//let string = prompt("please enter a string of your choice");
+
+//if (string.length > 1) {
+  //  let  firstChar = string[0];
+ //  console.log(firstChar);
+ //   let lastChar = string[string.length - 1];
+ //   console.log(lastChar);
+ //   let remainingString = string.substring(1,string.length - 1);
+ //   console.log(remainingString);
+ //  let result;
+ //  result =  lastChar + remainingString + lastChar + firstChar;
+ //  alert (result);
+ //  console.log(result);
+//} 
+
+   
